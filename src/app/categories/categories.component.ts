341612d22eaf522a4aa31476ee06a54656547096
atala15 <atala15@freeuni.edu.ge>
2020-07-25 10:06:13 +0400

    added category interface and inserted it in the root view

---
+++ src/app/categories/categories.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../category';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  category: Category = {
+    id: 1, 
+    name: 'Charity'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
